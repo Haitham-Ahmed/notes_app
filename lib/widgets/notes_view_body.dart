c986624a99881cbba410ead1757f5aed16020995
Your Name <you@example.com>
2025-02-22 09:01:58 +0200

    create notes list view

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/custom_note_item.dart';
+import 'package:notes_app/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -13,11 +14,11 @@ class NotesViewBody extends StatelessWidget {
       child: Column(
         children: const [
           SizedBox(height: 50),
-           CustomAppBar(),
-           NotesItem(),
+          CustomAppBar(),
+
+          Expanded(child: NotesListView(),),
         ],
       ),
     );
   }
 }
-
